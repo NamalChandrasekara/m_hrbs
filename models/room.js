5b34a6487bdf06914bc90df7cc5bf7925ffdab39
NamalChandrasekara <85227162+NamalChandrasekara@users.noreply.github.com>
2023-03-20 12:30:30 +0530

    Add files via upload

--- models/room.js
+++ models/room.js
@@ -1,1 +1,35 @@
+import mongoose from "mongoose";
 
+const Schema = mongoose.Schema;
+const roomSchema  = new Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    maxcount:{
+        type:Number,
+        required:true
+    },
+    phonenumber:{
+        type:Number,
+        required:true
+    },
+    rentperday:{
+        type:Number,
+        required:true
+    },
+    /*imagurl:[],
+    currentbookings:[],
+    type:{
+        type:String,
+        required:true
+    },*/
+    description :{
+        type:String,
+        required:true
+    }
+
+})
+
+const Room = mongoose.model('Room',roomSchema);
+module.exports=Room;
