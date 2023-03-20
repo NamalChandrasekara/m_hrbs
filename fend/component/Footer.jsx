53baef2736518302c759fa0b522732b5a25a65f9
NamalChandrasekara <85227162+NamalChandrasekara@users.noreply.github.com>
2023-03-20 10:24:14 +0530

    Add files via upload

---
+++ fend/component/Footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css"
+const Footer=()=>{
+    return(
+        <div className="footer">
+            <div className="fText">Copyright © 2023 HRBS.</div>
+        </div>
+        
+    )
+}
+export default Footer;
